f50acaed75b7070527c048501af2126ae5dd2a17
nivedks72 <37117180+nivedks72@users.noreply.github.com>
2020-07-29 01:15:36 +0530

    DATABSE_URL experiment

--- meenkari/secret_settings.py
+++ meenkari/secret_settings.py
@@ -18,6 +18,7 @@ DB_temp = {
     'ENGINE': 'django.db.backends.sqlite3',
     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 }
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True) if (os.environ.get('DATABASE_URL')) else DB_temp
 
 
