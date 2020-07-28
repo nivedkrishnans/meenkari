a48f4afa0185531551750416ffad40f4817f02b0
nivedks72 <37117180+nivedks72@users.noreply.github.com>
2020-07-29 00:34:55 +0530

    DATABASE_URL configuration experiement

--- meenkari/secret_settings.py
+++ meenkari/secret_settings.py
@@ -14,15 +14,9 @@ ALLOWED_HOSTS = ['127.0.0.1','meenkari.herokuapp.com']
 
 #if the DATABSE_URL variable is found like in heroku, it uses that, otherwise uses the default sqlite database
 import dj_database_url
-if (os.environ.get('DATABASE_URL')):
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+#if the DATABSE_URL variable is found like in heroku, it uses that, otherwise uses the default sqlite database
+import dj_database_url
+DATABASES = dj_database_url.config(conn_max_age=600, ssl_require=True) if (os.environ.get('DATABASE_URL')) else {'default':{'ENGINE': 'django.db.backends.sqlite3','NAME': os.path.join(BASE_DIR, 'db.sqlite3'),}}
 
 
 #for gmail, use the following
