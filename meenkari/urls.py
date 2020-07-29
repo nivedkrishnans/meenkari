ae037eb2ee5d8496a3c302e525fd2861df8d69d2
nivedks72 <37117180+nivedks72@users.noreply.github.com>
2020-07-29 05:46:11 +0530

    Shifted to 1-step registration without email

--- meenkari/urls.py
+++ meenkari/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import include, url
 from django.urls import path
 from . import views
 import django_registration
+from django_registration.backends.one_step.views import RegistrationView #not needed if using 2-step registration with email
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,6 +15,10 @@ urlpatterns = [
     path('error/', views.error, name="error"),
     path('unite/<str:url_id>', views.unite, name="unite"),
     path('play/<str:url_id>', views.play, name="play"),
+    
+    path('accounts/register/', RegistrationView.as_view(success_url='/'),name='django_registration_register'), #remove this to get 2-step registration with email
+    path('accounts/', include('django_registration.backends.one_step.urls')), #remove this to get 2-step registration with email
+    
     path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     #trial and test pages below
