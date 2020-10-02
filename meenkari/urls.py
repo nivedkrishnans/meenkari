118835a680d472a9f564c45876b0e8ae79c162af
nivedkrishnans <37117180+nivedkrishnans@users.noreply.github.com>
2020-10-02 12:59:49 +0530

    Removed test and trial  pages

--- meenkari/urls.py
+++ meenkari/urls.py
@@ -24,17 +24,5 @@ urlpatterns = [
     
     path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    
-    path('tester/', views.tester, name="tester"),
-    
-    #test page for gsu
-    path('gsu/<str:url_id>', views.gsu, name="gsu"),
-    
-    #trial and test pages below
 
-    path('start/', views.start, name="start"),
-    path('game/<str:url_id>', views.game, name="game"),
-    path('communication_test', views.communication_test, name="communication_test"),
-    path('communication_test2', views.communication_test2, name="communication_test2"),
-    path('communication_test2_trigger', views.communication_test2_trigger, name="communication_test2_trigger"),
 ]
