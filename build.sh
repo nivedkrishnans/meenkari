17d287bd9f6d4c41c2f892956f8d177e35497945
nivedkrishnans <37117180+nivedkrishnans@users.noreply.github.com>
2023-07-11 00:53:37 +0530

    Trying to make a more comprehensive build statement

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ set -o errexit
 
 python -m pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+python manage.py createsuperuser  --no-input
