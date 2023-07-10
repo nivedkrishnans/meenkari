c07dc08e183b06a10730bea80d44ca5fa14ea3d6
nivedkrishnans <37117180+nivedkrishnans@users.noreply.github.com>
2023-07-11 01:39:37 +0530

    prevent error when default superuser already exists

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 # The default password for the initial superuser comes from env variable DJANGO_SUPERUSER_PASSWORD
-python manage.py createsuperuser --no-input --username meenkari --email meenkari@meenkari.com 
+python manage.py createsuperuser --no-input --username meenkari --email meenkari@meenkari.com || { echo "default superuser already exists"; }
