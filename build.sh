8ebb2b2324ae1a4037269e605f5e8da9d1f226c5
nivedkrishnans <37117180+nivedkrishnans@users.noreply.github.com>
2023-07-11 01:15:57 +0530

    Added dummy superuser

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ python -m pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-python manage.py createsuperuser  --no-input
+# The default password for the initial superuser comes from env variable DJANGO_SUPERUSER_PASSWORD
+python manage.py createsuperuser --no-input --username meenkari --email meenkari@meenkari.com 
